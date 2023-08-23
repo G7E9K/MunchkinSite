539a4c54e48aeb93c340c599ebc0d0d74060fc07
G7E9K <mr.evgenii.b@yandex.ru>
2023-08-23 15:46:00 +0300

    first commit

---
+++ score.js
@@ -0,0 +1,39 @@
+function level_plus(i){
+	var elem = document.getElementsByClassName("level")[i-1];
+	elem.textContent = Number(elem.textContent) + 1;
+	if(elem.textContent==10){
+		name = document.getElementById("name" + i).value;
+		alert(name + " победил!");
+	}
+	power_plus(i);
+}
+
+function level_minus(i){
+	var elem = document.getElementsByClassName("level")[i-1];
+	if(elem.textContent!=1){
+		elem.textContent = Number(elem.textContent) - 1;
+	}
+	power_minus(i);
+}
+
+function stuff_plus(i){
+	var elem = document.getElementsByClassName("stuff")[i-1];
+	elem.textContent = Number(elem.textContent) + 1;
+	power_plus(i);
+}
+
+function stuff_minus(i){
+	var elem = document.getElementsByClassName("stuff")[i-1];
+	elem.textContent = Number(elem.textContent) - 1;
+	power_minus(i);
+}
+
+function power_plus(i){
+	var elem = document.getElementsByClassName("power")[i-1];
+	elem.textContent = Number(elem.textContent) + 1;
+}
+
+function power_minus(i){
+	var elem = document.getElementsByClassName("power")[i-1];
+	elem.textContent = Number(elem.textContent) - 1;
+}
